3fa1c266c7a98d3036ad274e7238e12f1dddd512
Alexandr Matveev <kareroyal@gmail.com>
2022-06-05 17:07:29 +0300

    https

--- components/Weather/CurrentWeather.tsx
+++ components/Weather/CurrentWeather.tsx
@@ -6,7 +6,7 @@ function CurrentWeather({ dot }: any) {
   useEffect(() => {
     async function getWeather() {
       let response = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?lat=${dot.coords[0]}&lon=${dot.coords[1]}&appid=7f6b4081e42acb33bcce11a03f4d3526`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${dot.coords[0]}&lon=${dot.coords[1]}&appid=7f6b4081e42acb33bcce11a03f4d3526`
       );
       const currentWeather = await response.json();
       setWeather(currentWeather);
