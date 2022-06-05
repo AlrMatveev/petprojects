d8421b615160fe46f3504dd0fc1079ab078a8dea
Alexandr Matveev <kareroyal@gmail.com>
2022-06-05 15:50:34 +0300

    ymaps

--- components/Wather/ymaps.js
+++ components/Wather/ymaps.js
@@ -1,18 +1,20 @@
 import { useEffect } from "react";
 
-function Ymaps() {
+function Ymaps({ setCoords }) {
   useEffect(() => {
     function loadScript(src, callback) {
       let script = document.createElement("script");
       script.src = src;
 
-      script.onload = () => callback(script);
+      script.onload = () => {
+        callback(script);
+      };
 
       document.head.append(script);
     }
 
     loadScript(
-      "https://api-maps.yandex.ru/2.1/?apikey=b6c08aa6-dd71-4a2e-b53d-01c79b3f1e83&lang=ru_RU",
+      "https://api-maps.yandex.ru/2.1.78/?apikey=b6c08aa6-dd71-4a2e-b53d-01c79b3f1e83&lang=ru_RU",
       function () {
         ymaps.ready(init);
       }
@@ -22,22 +24,83 @@ function Ymaps() {
     // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
 
     function init() {
-      // Создание карты.
-      var myMap = new ymaps.Map("map", {
-        // Координаты центра карты.
-        // Порядок по умолчанию: «широта, долгота».
-        // Чтобы не определять координаты центра карты вручную,
-        // воспользуйтесь инструментом Определение координат.
-        center: [55.76, 37.64],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
-        zoom: 7,
+      var myPlacemark,
+        myMap = new ymaps.Map(
+          "map",
+          {
+            center: [55.755864, 37.617698],
+            zoom: 10,
+          },
+          {
+            searchControlProvider: "yandex#search",
+          }
+        );
+
+      // Слушаем клик на карте.
+      myMap.events.add("click", function (e) {
+        var coords = e.get("coords");
+        setCoords(coords);
+
+        // Если метка уже создана – просто передвигаем ее.
+        if (myPlacemark) {
+          myPlacemark.geometry.setCoordinates(coords);
+        }
+        // Если нет – создаем.
+        else {
+          myPlacemark = createPlacemark(coords);
+          myMap.geoObjects.add(myPlacemark);
+          // Слушаем событие окончания перетаскивания на метке.
+          myPlacemark.events.add("dragend", function () {
+            getAddress(myPlacemark.geometry.getCoordinates());
+          });
+        }
+
+        getAddress(coords);
       });
+
+      // Создание метки.
+      function createPlacemark(coords) {
+        return new ymaps.Placemark(
+          coords,
+          {
+            iconCaption: "поиск...",
+          },
+          {
+            preset: "islands#violetDotIconWithCaption",
+            draggable: true,
+          }
+        );
+      }
+
+      // Определяем адрес по координатам (обратное геокодирование).
+      function getAddress(coords) {
+        myPlacemark.properties.set("iconCaption", "поиск...");
+        ymaps.geocode(coords).then(function (res) {
+          var firstGeoObject = res.geoObjects.get(0);
+
+          myPlacemark.properties.set({
+            // Формируем строку с данными об объекте.
+            iconCaption: [
+              // Название населенного пункта или вышестоящее административно-территориальное образование.
+              firstGeoObject.getLocalities().length
+                ? firstGeoObject.getLocalities()
+                : firstGeoObject.getAdministrativeAreas(),
+              // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
+              firstGeoObject.getThoroughfare() || firstGeoObject.getPremise(),
+            ]
+              .filter(Boolean)
+              .join(", "),
+            // В качестве контента балуна задаем строку с адресом объекта.
+            balloonContent: firstGeoObject.getAddressLine(),
+          });
+        });
+      }
     }
   });
+
   return (
     <>
-      <div id="map" style={{ width: "600px", height: "400px" }}></div>
+      <div id="map" style={{ width: "100%", height: "400px" }}></div>
     </>
   );
 }
--- pages/wather.tsx
+++ pages/wather.tsx
@@ -3,24 +3,20 @@ import Head from "next/head";
 import Script from "next/script";
 import { useEffect } from "react";
 import Navigation from "../components/Navigation";
-import Ymaps from "../components/Wather/ymaps";
+import Wather from "../components/Wather/Wather";
 
-const Wather: NextPage = () => {
+const WatherPage: NextPage = () => {
   return (
     <>
       <Head>
         <title>Wather</title>
         <meta name="description" content="Wather" />
         <link rel="icon" href="/favicon.ico" />
-        <script
-          src="https://api-maps.yandex.ru/2.1/?apikey=b6c08aa6-dd71-4a2e-b53d-01c79b3f1e83&lang=ru_RU"
-          type="text/javascript"
-        ></script>
       </Head>
       <Navigation />
-      <Ymaps />
+      <Wather />
     </>
   );
 };
 
-export default Wather;
+export default WatherPage;
